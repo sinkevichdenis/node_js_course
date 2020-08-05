bacb44adba6d34f75029ab8f2ec5c3e426a5ae6b
Sinkevich Dzianis <dzianis_sinkevich1@epam.com>
2020-08-05 16:37:22 +0300

    fix(lesson1): add updates

--- lesson_01/task_01/index.js
+++ lesson_01/task_01/index.js
@@ -1,16 +1,3 @@
-/*
-ES-5 version
-
-function reverseString (string) {
-    return string.split('').reverse().join('');
-}
-
-process.stdin.on('data', function (data) {
-    process.stdout.write(reverseString (data.toString()));
-});*/
-
-const reverseString = string => string.split('').reverse().join('');
-
 process.stdin.on('data', data => {
-    console.log(reverseString (data.toString()));
+    console.log(data.reverse().toString());
 });
--- lesson_01/task_02/index.js
+++ lesson_01/task_02/index.js
@@ -11,13 +11,20 @@ const onNext = json => {
     console.log(json)
 };
 
+const PATH_WRITTEN_BY_LINE = './assets/fromCsvByLine.txt';
+const PATH_WRITTEN_WHOLE = './assets/fromCsvWhole.txt';
+
 const rStream = fs.createReadStream('./assets/csv/nodejs-hw1-ex1.csv');
-const wStream = fs
-    .createWriteStream('./assets/fromCSV.txt')
+const createWStream = path => fs
+    .createWriteStream(path)
     .on('error', onError)
     .on('finish', onComplete);
 
+// written line by line
 csv()
     .fromStream(rStream)
     .subscribe(onNext, onError)
-    .pipe(wStream);
+    .pipe(createWStream(PATH_WRITTEN_BY_LINE));
+
+// written whole
+rStream.pipe(csv()).pipe(createWStream(PATH_WRITTEN_WHOLE));
