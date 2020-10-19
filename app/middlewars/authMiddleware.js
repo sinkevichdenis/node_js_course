5aa46ff40849fd90ca95206d994850723382fb8e
Sinkevich Dzianis <dzianis_sinkevich1@epam.com>
2020-10-19 13:56:43 +0300

    fix(lesson6): remove console.log

--- app/middlewars/authMiddleware.js
+++ app/middlewars/authMiddleware.js
@@ -4,11 +4,9 @@ import jwt from 'jsonwebtoken';
 export const authMiddleware = (req, res, next) => {
     const token = req.headers['x-token'];
     if (!token) {
-        console.log('NO TOKEN');
         res.sendStatus(401);
     } else {
         const secret = config.get('privateKey');
-        console.log('NO TOKEN @@@@@');
         jwt.verify(token, secret, (err) => (
             err ? res.sendStatus(403) : next()
         ));
